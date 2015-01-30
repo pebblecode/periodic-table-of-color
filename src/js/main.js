64f00a65f78718d26a79d187ae9f260cc9b5dcd8
Mark Durrant <mark.j.durrant.gmail.com>
2015-01-30 10:54:53 +0000

    create color data

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,37 @@
 (function () { 'use strict';
 
+  // var colorList =  $( '#js-color-list' );
+
+  // $.getJSON( "../data/colors.json", function ( data ) {
+
+  //   for( var i = 0; i < data.length; i++ ) {
+  //       var name = data[ i ].name,
+  //           hex = data[ i ].value;
+  //       var color = tinycolor( data[ i ].value ),
+  //           r = color.toRgb().r,
+  //           g = color.toRgb().g,
+  //           b = color.toRgb().b,
+  //           h = color.toHsl().h,
+  //           s = color.toHsl().s,
+  //           l = color.toHsl().l,
+  //           v = color.toHsv().v;
+
+  //     colorList.append(
+  //       "<li class='color' style='background-color:" + hex + "'>" + name + "," + hex + "," + h + "," + s + "," + l + "," + v + "," + r + "," + g + "," + b + "</li>"
+  //     );
+
+  //     // colorList.append(
+  //     //   '<li class="color-item" style="background-color:' + hex + '"> <span class="color-name">' + name + '</span> </li>'
+  //     // );
+  //   }
+  // });
+
+  // // var red = tinycolor( 'red' );
+  // // var h = red.toHsl().h;
+
+
+  // // console.log( h );
+
+  console.log( colorData );
+
 }()); // end 'use strict'
